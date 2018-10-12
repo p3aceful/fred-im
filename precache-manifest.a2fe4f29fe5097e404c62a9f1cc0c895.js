af28bb6566b87d3cfb8233324494950da21e30ef
P3aceful <kptfredrik@gmail.com>
2018-10-13 00:19:14 +0200

    Updates

--- precache-manifest.a2fe4f29fe5097e404c62a9f1cc0c895.js
+++ precache-manifest.a2fe4f29fe5097e404c62a9f1cc0c895.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fred-im/static/js/runtime~main.318babfa.js"
   },
   {
-    "revision": "0b95f24c3ff623c6eb9f",
-    "url": "/fred-im/static/js/main.0b95f24c.chunk.js"
+    "revision": "bf312e1b9399d5519df4",
+    "url": "/fred-im/static/js/main.bf312e1b.chunk.js"
   },
   {
     "revision": "ad4591f84e9b761fa829",
     "url": "/fred-im/static/js/1.ad4591f8.chunk.js"
   },
   {
-    "revision": "0b95f24c3ff623c6eb9f",
+    "revision": "bf312e1b9399d5519df4",
     "url": "/fred-im/static/css/main.eafa6462.chunk.css"
   },
   {
-    "revision": "9beb41f2498589dc1f76813966105e7a",
+    "revision": "3605c8d21e77f0dd1f2840c3e6a2def3",
     "url": "/fred-im/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/fred-im/precache-manifest.4e07ea1a15d2a391ae99ed72b0d9885d.js"
+  "/fred-im/precache-manifest.a2fe4f29fe5097e404c62a9f1cc0c895.js"
 );
 
 workbox.clientsClaim();
