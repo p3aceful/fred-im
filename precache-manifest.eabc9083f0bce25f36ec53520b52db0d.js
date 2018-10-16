efee0e243bf1fad275525a3a08c1b03382b4d192
P3aceful <kptfredrik@gmail.com>
2018-10-16 19:58:26 +0200

    Updates

---
+++ precache-manifest.eabc9083f0bce25f36ec53520b52db0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "318babfa46792e8f1a77",
+    "url": "/fred-im/static/js/runtime~main.318babfa.js"
+  },
+  {
+    "revision": "14a03e7636e26a606f84",
+    "url": "/fred-im/static/js/main.14a03e76.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/fred-im/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "14a03e7636e26a606f84",
+    "url": "/fred-im/static/css/main.eafa6462.chunk.css"
+  },
+  {
+    "revision": "e3ef389d36cd6303ec3c98304729f5de",
+    "url": "/fred-im/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/freddIm/precache-manifest.42a61346d2a2df953a4cfddb3740442f.js"
+  "/fred-im/precache-manifest.eabc9083f0bce25f36ec53520b52db0d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/freddIm/index.html", {
+workbox.routing.registerNavigationRoute("/fred-im/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
