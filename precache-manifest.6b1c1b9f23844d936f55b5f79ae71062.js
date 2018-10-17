7bf9860d021c27ded42bc329e0337d0d9f063b0e
P3aceful <kptfredrik@gmail.com>
2018-10-17 16:50:18 +0200

    Updates

--- precache-manifest.6b1c1b9f23844d936f55b5f79ae71062.js
+++ precache-manifest.6b1c1b9f23844d936f55b5f79ae71062.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/fred-im/static/js/runtime~main.318babfa.js"
   },
   {
-    "revision": "14a03e7636e26a606f84",
-    "url": "/fred-im/static/js/main.14a03e76.chunk.js"
+    "revision": "b9ab8670f6572d3d2016",
+    "url": "/fred-im/static/js/main.b9ab8670.chunk.js"
   },
   {
     "revision": "ad4591f84e9b761fa829",
     "url": "/fred-im/static/js/1.ad4591f8.chunk.js"
   },
   {
-    "revision": "14a03e7636e26a606f84",
+    "revision": "b9ab8670f6572d3d2016",
     "url": "/fred-im/static/css/main.eafa6462.chunk.css"
   },
   {
-    "revision": "e3ef389d36cd6303ec3c98304729f5de",
+    "revision": "fbc854d7143aa3fa061f94d864311137",
     "url": "/fred-im/index.html"
   }
 ];
