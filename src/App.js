757590e839773e1142f681ea20fa90749c5a27c8
P3aceful <kptfredrik@gmail.com>
2018-10-29 00:10:57 +0100

    refactor the connectionlayer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Chat from './Chat.js';
 import Header from './components/Header.js';
-import ConnectionManager from './ConnectionManager.js';
+import Connection from './Connection.js';
 
 
 class App extends Component {
@@ -10,60 +10,73 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = { messages: [], users: [], typers: [], isOpen: false };
-		this.connectionManager = null;
+		this.connection = null;
 
 		this.send = this.send.bind(this);
-	}
-
-	addToTypers(userid) {
-		this.setState(prev => ({
-			typers: [...prev.typers, userid]
-		}));
+		this.toggleOpenState = this.toggleOpenState.bind(this);
 	}
 
 	componentDidMount() {
 		const url = this.getServerUrl();
-        this.connectionManager = new ConnectionManager(this);
-		this.connectionManager.connect(url);
-		// setTimeout(this.connectionManager.connect, 1000, url); // Fake delay
-        this.pingTimeoutId = setTimeout(this.send, 30000, { type: 'ping' });
-	}
+		this.connection = new Connection(url);
 
-	removeFromTypers(userid) {
-		this.setState(prev => ({
-			typers: prev.typers.filter(typerid => typerid !== userid)
-		}));
+		this.connection.events.listen('open', () => {
+			console.log('Connection established!');
+			this.toggleOpenState();
+		});
+
+		this.connection.events.listen('chat', data => {
+			const { userid, message, date } = data;
+			this.setState(prev => ({
+				messages: [...prev.messages, { userid, message, date }]
+			}));
+		});
+
+		this.connection.events.listen('chat_history', data => {
+			console.log('Received a chat log of', data.messages.length, 'messages.');
+			const messages = data.messages;
+			this.setState({ messages });
+		});
+
+		this.connection.events.listen('server_broadcast', data => {
+			const me = data.peers.you;
+			const others = data.peers.clients;
+			const set = new Set([me, ...others.map(o => o.id)]);
+			this.setState({ users: [...set] });
+		});
+
+		this.connection.events.listen('user_started_typing', data => {
+			console.log('A user started typing');
+			const userid = data.userid;
+			this.setState(prev => ({
+				typers: [...prev.typers, userid]
+			}));
+		});
+
+		this.connection.events.listen('user_stopped_typing', data => {
+			console.log('A user stopped typing');
+
+			const userid = data.userid;
+			this.setState(prev => ({
+				typers: prev.typers.filter(typerid => typerid !== userid)
+			}));
+		});
 	}
 
 	getServerUrl() {
-        if (window.location.hostname === 'localhost') {
-            return 'ws://localhost:9876';
-        }
-        else {
-            return 'wss://fred-im.herokuapp.com';
-        }
-	}
-
-	receiveChatHistory(messages) {
-		this.setState({ messages });
-	}
-
-	receiveMessage(userid, msg, date) {
-		this.setState(prev => ({
-			messages: [...prev.messages, { userid, message: msg, date }]
-		}));
-	}
-
-	receiveActiveUsers(users) {
-		const set = new Set(users);
-		this.setState({ users: [...set] });
+		if (window.location.hostname === 'localhost') {
+			return 'ws://localhost:9876';
+		}
+		else {
+			return 'wss://fred-im.herokuapp.com';
+		}
 	}
 
 	render() {
 		return (
 			<div className="App d-flex flex-column" style={{ height: '100vh' }}>
 				<Header users={this.state.users} />
-				<Chat 
+				<Chat
 					users={this.state.users}
 					typers={this.state.typers}
 					messages={this.state.messages}
@@ -75,9 +88,7 @@ class App extends Component {
 	}
 
 	send(data) {
-		clearTimeout(this.pingTimeoutId);
-		this.pingTimeoutId = setTimeout(this.send, 30000, { type: 'ping' });
-		this.connectionManager.send(data);
+		this.connection.send(data);
 	}
 
 	toggleOpenState() {
---
+++ src/Connection.js
@@ -0,0 +1,23 @@
+import Events from './Events.js';
+
+export default class Connection {
+
+    constructor(address) {
+        this.events = new Events();
+        this.connection = new WebSocket(address);
+        
+        this.connection.addEventListener('open', () => {
+            this.events.emit('open');
+        });
+        
+        this.connection.addEventListener('message', event => {
+            const message = JSON.parse(event.data);
+            this.events.emit(message.type, message.data);
+        });
+    }
+
+    send(data) {
+        const message = JSON.stringify(data);
+        this.connection.send(message);
+    }
+}
